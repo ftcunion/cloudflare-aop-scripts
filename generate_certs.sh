4208531d93f60ddd44fa0bb3ebc78eeb43343b3b
Matthew Thomas <matthew@mwt.me>
2025-07-17 10:39:08 -0300

    Change exp tp 9999 and improve README

--- generate_certs.sh
+++ generate_certs.sh
@@ -7,7 +7,7 @@ openssl genrsa -aes256 -out rootca.key 4096
 echo "Create the CA root certificate. When prompted, fill in the information to be included in the certificate. For the Common Name field, use the domain name as value, not the hostname."
 echo "Press enter to continue..."
 read -r _
-openssl req -x509 -new -nodes -key rootca.key -sha256 -days 1826 -out rootca.crt
+openssl req -x509 -new -nodes -key rootca.key -sha256 -days 9999 -out rootca.crt
 
 echo "Create a Certificate Signing Request (CSR). When prompted, fill in the information to be included in the request. For the Common Name field, use the hostname as value."
 echo "Press enter to continue..."
@@ -18,5 +18,5 @@ echo "Sign the certificate using the rootca.key and rootca.crt created in previo
 echo "Press enter to continue..."
 read -r _
 echo 'basicConstraints=CA:FALSE' > ./cert.v3.ext
-openssl x509 -req -in cert.csr -CA rootca.crt -CAkey rootca.key -CAcreateserial -out cert.crt -days 730 -sha256 -extfile ./cert.v3.ext
+openssl x509 -req -in cert.csr -CA rootca.crt -CAkey rootca.key -CAcreateserial -out cert.crt -days 9999 -sha256 -extfile ./cert.v3.ext
 echo "Certificate and key files have been generated."
